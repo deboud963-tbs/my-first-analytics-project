2a18e681feaa9e2afcdb6c01db9f962955b0db67
deboud963-tbs <d.boudet@tbs-education.org>
2025-03-20 15:53:32 +0000

    update

--- app.py
+++ app.py
@@ -32,3 +32,17 @@ with st.expander("See explanation"):
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title("Manipulate Streamlit Chart")
+
+#Generate random data
+bar_data = pd.DataFrame(np.random.randn(20,3), columns = ["a", "b", "c"])
+st.bar_chart(bar_data)
+
+#Generate random data for line chart
+line_data = pd.DataFrame(np.random.randn(20,3), columns=["a","b","c"])
+st.line_chart(line_data)
+
+#Generate random data for scatter chart
+chart_data = pd.DataFrame(np.random.randn(20,3), columns = ["a","b","c"])
+st.scatter_chart(chart_data)
